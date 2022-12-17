6ec151809d07b50cff790e489c58fc7ddd493049
Андрей Лаврухин <lavruhinaa@gmail.com>
2022-12-17 13:37:02 +0600

    Server gets data from Adam and Owen modules + GPS

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ command_get_data = \
      "Loco01": "#03\r",
      "Loco02": "#04\r"}
 current_data = dict()
-mirrors_queues = dict()
 vals = [[0, 0, 0], [0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]
 
 
@@ -33,7 +32,7 @@ async def handle_connection(reader, writer):
     tail = ""
     while True:
         # Register a client in the list
-        if adr not in clients:  # and adr not in mirrors_queues:
+        if adr not in clients:
             # Request to detect who is it
             for sync_data in ["$01M\r", "$02M\r", "$03M\r", "$04M\r"]:
                 print(f"Send to {adr}: {sync_data}")
@@ -54,35 +53,13 @@ async def handle_connection(reader, writer):
                 print("Next attempt by timeout")
                 continue
             print(f"Received from {adr}: {received_data}")
-            # Answer from echo mirror
-            if received_data == b'ECHO':
-                mirrors_queues[adr] = queue.Queue(20)
             # Answer from measurer
-            elif received_data in points:
+            if received_data in points:
                 point_number = points[received_data][0]
                 clients[adr] = points[received_data][1]
         elif adr in clients:
-            # dt = datetime.datetime.now()
-            # del_list = []
-            # for key in current_data:
-            #     if dt - datetime.timedelta(seconds=10) > key:
-            #         del_list.append(key)
-            # for key in del_list:
-            #     del current_data[key]
             sync_data = command_get_data[clients[adr]]
             try:
-                # last_k = None
-                # last_v = None
-                # if len(current_data) > 0:
-                #     for key in current_data:
-                #         value = current_data[key]
-                #         if value["num"] == 4:
-                #             if last_k is None:
-                #                 last_k = key
-                #                 last_v = value
-                #             if key > last_k:
-                #                 last_k = key
-                #                 last_v = value
                 if clients[adr][0:3] == "Sta":
                     data_to_send = sync_data
                 else:
@@ -94,9 +71,6 @@ async def handle_connection(reader, writer):
                                    f" {vals[3][0]:1.0f} {vals[3][1]:1.2f} {vals[3][2]:1.2f}" + \
                                    f" {vals[3][3]:1.0f} {vals[3][4]:1.2f} {vals[3][5]:1.2f} {vals[3][6]:1.4f} \r"
                     print(data_to_send)
-                # if last_k is not None:
-                #     data_to_send += last_v["Sta01"] + last_v["Sta02"] + last_v["Loco01"] + last_v["Loco02"]
-                #     print(f"LAST: {str(last_k)}")
                 writer.write(data_to_send.encode())
                 await writer.drain()
             except (ConnectionError, OSError):
@@ -136,24 +110,10 @@ async def handle_connection(reader, writer):
                             point = 3
                         if point <= 1:
                             vals[point] = [float(message_vals[3]), float(message_vals[4]), float(message_vals[5])]
-                        # elif point == 2:
-                        #     vals[point] = [float(message_vals[3]), float(message_vals[4]), float(message_vals[5]),
-                        #                    float(message_vals[8]), 0, float(message_vals[6]), float(message_vals[7])]
                         else:
                             vals[point] = [float(message_vals[3]), float(message_vals[4]), float(message_vals[5]),
                                            float(message_vals[8]), float(message_vals[9]), float(message_vals[6]),
                                            float(message_vals[7])]
-                        # date = message_vals[1].split('-')
-                        # time = message_vals[2].split(':')
-                        # dt = datetime.datetime(year=int(date[0]), month=int(date[1]), day=int(date[2]),
-                        #                        hour=int(time[0]), minute=int(time[1]), second=int(time[2]))
-                        # if dt in current_data:
-                        #     current_data[dt]["num"] += 1
-                        #     current_data[dt][clients[adr]] = message_str
-                        # else:
-                        #     current_data[dt] = dict()
-                        #     current_data[dt]["num"] = 1
-                        #     current_data[dt][clients[adr]] = message_str
                     except (UnicodeError, ValueError):
                         print('Value Error in received data')
             except (ConnectionError, asyncio.TimeoutError):
@@ -161,16 +121,6 @@ async def handle_connection(reader, writer):
             if timeout_cnt >= 2:
                 print("Disconnect by timeout")
                 del clients[adr]
-                break
-        if adr in mirrors_queues:
-            try:
-                while not mirrors_queues[adr].empty():
-                    data = mirrors_queues[adr].get()
-                    print(f"Get {data} from mirror")
-                    writer.write(data.encode())
-                    await writer.drain()
-            except ConnectionError:
-                print(f"Client suddenly closed, cannot send")
                 break
     writer.close()
     print("Disconnected by", adr)
